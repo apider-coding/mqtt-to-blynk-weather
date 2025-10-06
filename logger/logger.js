68e8f2f1d155a259b93428b12f0fcea4356bedbb
apider <filipk73@gmail.com>
2025-10-06 22:40:52 +0200

    fix: update winston

--- logger/logger.js
+++ logger/logger.js
@@ -3,19 +3,27 @@
 const winston = require('winston');
 
 const logger = winston.createLogger({
-  level: 'debug',
+  // level: 'debug',
   transports: [
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
-      // format: winston.format.combine(
-      //   winston.format.timestamp(),
-      //   winston.format.splat(),
-      //   winston.format.simple(),
-      // ),
+      format: winston.format.combine(),
     })],
 });
+
+// const logger = winston.createLogger({
+//   level: 'debug',
+//   transports: [
+//     new winston.transports.Console({
+//       format: winston.format.combine(
+//         winston.format.timestamp(),
+//         winston.format.json(),
+//       ),
+//       // format: winston.format.combine(
+//       //   winston.format.timestamp(),
+//       //   winston.format.splat(),
+//       //   winston.format.simple(),
+//       // ),
+//     })],
+// });
 
 module.exports = logger;
