ce20afe8adc1b41a7ebf8e8f427df4727c5a1775
Fille <filipk73@gmail.com>
2025-10-07 18:15:03 +0200

    fix: remove node-fetch

--- postBlynk.js
+++ postBlynk.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable no-console */
-// const fetch = require('node-fetch');
 const axios = require('axios');
 // const { trace } = require('@opentelemetry/api');
 const logger = require('./logger/logger');
@@ -12,15 +11,10 @@ const logger = require('./logger/logger');
 const postBlynk = async (url) => {
   try {
     const resp = await axios.get(url, { method: 'GET' });
-    // For fetch need to check resp.ok
-    // if (!resp.ok) {
-    //   throw Error(resp);
-    // }
     logger.info({ message: 'Posted to blynk', url, response_status: resp.status });
     return true;
   } catch (err) {
     logger.error({ message: err.message });
-    // logger.error('This is a error message'); // A message could be logged like this as well
     return err;
   }
 };
