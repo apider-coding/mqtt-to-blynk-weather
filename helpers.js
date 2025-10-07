cda13e3b172606c18c6ef178f746cdcc91f6f9bc
Fille <filipk73@gmail.com>
2025-10-07 18:16:04 +0200

    fix: order

--- helpers.js
+++ helpers.js
@@ -9,9 +9,9 @@
  */
 
 const opentelemetry = require('@opentelemetry/api');
+const logger = require('./logger/logger');
 
 const tracer = opentelemetry.trace.getTracer('mqtt-to-blynk-weather');
-const logger = require('./logger/logger');
 
 const convertToMS = (item) => ((Number(item.toString()) * 1000) / 3600).toFixed(1);
 
