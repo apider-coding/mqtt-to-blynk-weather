5ca79404e974f6801c376b2ca1bb459b277c441a
Fille <filipk73@gmail.com>
2025-10-08 17:10:45 +0200

    fix; test better instrumentation

--- subscribeTopics.js
+++ subscribeTopics.js
@@ -1,28 +1,39 @@
 const opentelemetry = require('@opentelemetry/api');
 
 const tracer = opentelemetry.trace.getTracer('mqtt-to-blynk-weather');
-/* eslint-disable no-console */
-/**
- * @module
- */
-/**
- * @param {Object} client MQTT client
- * @param {Array} topics Array of topics
- */
 const logger = require('./logger/logger');
 
-const subscribeTopics = async (client, topics) => {
-  topics.forEach(async (topic) => {
+/**
+ * Subscribes to an array of MQTT topics.
+ * @param {Object} client MQTT client
+ * @param {Array} topics Array of topics to subscribe to
+ */
+const subscribeTopics = (client, topics) => {
+  tracer.startActiveSpan('subscribe-all-topics', async (span) => {
     try {
-      tracer.startActiveSpan(`subscribe-mqtt-topic:${topic.name}`, async (span) => {
-        await client.subscribe(topic.name);
-        logger.info({ message: 'Subscribed to topic', topic: topic.name });
-        span.end();
+      span.setAttribute('app.topics.count', topics.length);
+
+      const subscriptionPromises = topics.map((topic) => {
+        logger.info({ message: 'Subscribing to topic', topic: topic.name });
+        // Add an event for each subscription attempt
+        span.addEvent('Subscribing to topic', { name: topic.name });
+        return client.subscribe(topic.name);
       });
+
+      await Promise.all(subscriptionPromises);
+
+      logger.info({ message: 'All topics subscribed successfully' });
+      span.setStatus({ code: opentelemetry.SpanStatusCode.OK });
     } catch (err) {
-      logger.error(err.message);
+      logger.error({ message: 'Failed to subscribe to topics', error: err.message });
+      span.recordException(err);
+      span.setStatus({ code: opentelemetry.SpanStatusCode.ERROR, message: err.message });
+      // Re-throw the error if you want the caller to be able to handle it
+      throw err;
+    } finally {
+      // The span will be ended automatically when the promise resolves or rejects
+      span.end();
     }
   });
 };
-
 module.exports = { subscribeTopics };
