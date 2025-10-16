d80f604985cbf9988f956a6db73b7b1e47b6b98c
apider <filipk73@gmail.com>
2025-10-16 19:18:00 +0200

    fix: scitch to winston

--- logger/logger.js
+++ logger/logger.js
@@ -1,19 +1,20 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const pino = require('pino');
-
-module.exports = pino();
+// const pino = require('pino');
+// module.exports = pino();
 
 // const logger = require('pino')();
-
 // module.exports = logger;
-// const winston = require('winston');
 
-// const logger = winston.createLogger({
-//   level: 'info',
-//   transports: [
-//     new winston.transports.Console(),
-//   ],
-// });
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
+
+module.exports = logger;
 
 // // eslint-disable-next-line import/no-unresolved
 // const pino = require('pino');
